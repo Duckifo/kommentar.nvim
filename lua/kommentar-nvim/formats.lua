3ce564fa78a2db4c555d9caa4d9be1ae0a7a631b
Duckifo <cj.boqvist@gmail.com>
2025-02-18 17:15:03 +0100

    feat: added `reload_keys` to `formats.lua`

--- lua/kommentar-nvim/formats.lua
+++ lua/kommentar-nvim/formats.lua
@@ -1,38 +1,38 @@
 -- Heyo there, this is just a file full with formats, looking for explaination do `:help kommentar.nvim-format`
 
-	--- A collections of hand crafter formats
-	--- Very nice B)
+--- A collections of hand crafter formats
+--- Very nice B)
 ---@class Formats
-	---`@duckifo`
-	---```lua
-		--- 50:
-		--- -- <=:-~~~~~~~~~~~~ ) @duckifo ( ~~~~~~~~~~~~-:=> --
-		--- 100:
-		--- -- <=:-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ) @duckifo ( ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-:=> --
- 	---```
+---`@duckifo`
+---```lua
+--- 50:
+--- -- <=:-~~~~~~~~~~~~ ) @duckifo ( ~~~~~~~~~~~~-:=> --
+--- 100:
+--- -- <=:-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ) @duckifo ( ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-:=> --
+---```
 ---@field def_single string
-	---`@duckifo`
-	---```lua
-		--- 100:
-		--- -- (>- ------------------<=>------------------ @duckifo ------------------<=>------------------ -<) --
-	---```
+---`@duckifo`
+---```lua
+--- 100:
+--- -- (>- ------------------<=>------------------ @duckifo ------------------<=>------------------ -<) --
+---```
 ---@field free_single string
-	---`@duckifo`
-	---```lua
-		--- 100:
-		--- -- <===:-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-:===> --
-		--- -- <:-                                        ( @duckifo )                                       -:> --
-		--- -- <===:-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-:===> --
-	---```
+---`@duckifo`
+---```lua
+--- 100:
+--- -- <===:-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-:===> --
+--- -- <:-                                        ( @duckifo )                                       -:> --
+--- -- <===:-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-:===> --
+---```
 ---@field def_triple string
 
----@type {formats: Formats, keys: string[]}
+---@type {formats: Formats, keys: string[], reload_keys: function}
 local M = {}
 
 ---@type Formats
 M.formats = {
 	-- single lines
-	def_single = ' <=:-%(?:50:~) %(c:label) %(?:50:~)-:=> ',
+	def_single = ' <=:-%(?:50:~) ) %(c:label) ( %(?:50:~)-:=> ',
 	free_single = ' (>- %(?:25:-)<=>%(?:25.05:-) %(c:label) %(?:25.05:-)<=>%(?:25:-) -<) ',
 
 	-- triple
@@ -40,9 +40,15 @@ M.formats = {
 }
 
 M.keys = {}
-for key, _ in pairs(M.formats) do
-	table.insert(M.keys, key)
+--- reload the keys from `M.formats`
+M.reload_keys = function()
+	for key, _ in pairs(M.formats) do
+		table.insert(M.keys, key)
+	end
 end
 
----@return {formats: Formats, keys: string[]} 
+-- load the current keys
+M.reload_keys()
+
+---@return {formats: Formats, keys: string[]}
 return M
